6731bdd120e65104482e6d7c145a10cc4c13c4ab
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-04-29 16:21:55 +0200

    Added black as a depency and CI step. Changed flow config

--- flow_py_sdk/cadence/encode.py
+++ flow_py_sdk/cadence/encode.py
@@ -5,12 +5,28 @@ from flow_py_sdk.cadence.types import Value
 
 
 class CadenceJsonEncoder(json.JSONEncoder):
-    def __init__(self, *, skipkeys: bool = ..., ensure_ascii: bool = ..., check_circular: bool = ...,
-                 allow_nan: bool = ..., sort_keys: bool = ..., indent: Optional[int] = ...,
-                 separators: Optional[Tuple[str, str]] = ..., default: Optional[Callable[..., Any]] = ...) -> None:
-        super().__init__(skipkeys=skipkeys, ensure_ascii=ensure_ascii, check_circular=check_circular,
-                         allow_nan=allow_nan, sort_keys=sort_keys, indent=indent, separators=separators,
-                         default=default)
+    def __init__(
+        self,
+        *,
+        skipkeys: bool = ...,
+        ensure_ascii: bool = ...,
+        check_circular: bool = ...,
+        allow_nan: bool = ...,
+        sort_keys: bool = ...,
+        indent: Optional[int] = ...,
+        separators: Optional[Tuple[str, str]] = ...,
+        default: Optional[Callable[..., Any]] = ...
+    ) -> None:
+        super().__init__(
+            skipkeys=skipkeys,
+            ensure_ascii=ensure_ascii,
+            check_circular=check_circular,
+            allow_nan=allow_nan,
+            sort_keys=sort_keys,
+            indent=indent,
+            separators=separators,
+            default=default,
+        )
 
     def default(self, o: Any) -> Any:
         if isinstance(o, Value):
@@ -23,5 +39,12 @@ def encode_arguments(arguments: list[Value]) -> list[bytes]:
         return []
     # the separators and the new line are there to get an identical json as the flow-go-sdk does (usually).
     # It doesnt need to be identical but it is convenient for comparative testing with the go-sdk.
-    return [(json.dumps(a, ensure_ascii=False, cls=CadenceJsonEncoder, separators=(',', ':')) + '\n').encode('utf-8')
-            for a in arguments]
+    return [
+        (
+            json.dumps(
+                a, ensure_ascii=False, cls=CadenceJsonEncoder, separators=(",", ":")
+            )
+            + "\n"
+        ).encode("utf-8")
+        for a in arguments
+    ]
--- flow_py_sdk/templates.py
+++ flow_py_sdk/templates.py
@@ -5,21 +5,30 @@ from flow_py_sdk.signer import AccountKey
 from flow_py_sdk.tx import Tx
 
 
-def create_account_template(*, keys: list[AccountKey],
-                            contracts: dict[Annotated[str, "name"], Annotated[str, "source"]] = None) -> Tx:
+def create_account_template(
+    *,
+    keys: list[AccountKey],
+    contracts: dict[Annotated[str, "name"], Annotated[str, "source"]] = None
+) -> Tx:
     if keys:
         cadence_public_keys = cadence.Array([cadence.String(k.hex()) for k in keys])
     else:
         cadence_public_keys = cadence.Array([])
     if contracts:
         cadence_contracts = cadence.Dictionary(
-            [cadence.KeyValuePair(cadence.String(k), cadence.String(v.encode('utf-8').hex()))
-             for (k, v) in contracts.items()])
+            [
+                cadence.KeyValuePair(
+                    cadence.String(k), cadence.String(v.encode("utf-8").hex())
+                )
+                for (k, v) in contracts.items()
+            ]
+        )
     else:
         cadence_contracts = cadence.Dictionary([])
 
-    tx = Tx(
-        code="""
+    tx = (
+        Tx(
+            code="""
             transaction(publicKeys: [String], contracts:{String: String}) {
                 prepare(signer: AuthAccount) {
                     let acct = AuthAccount(payer: signer)
@@ -33,8 +42,10 @@ def create_account_template(*, keys: list[AccountKey],
                     }
                 }
             }
-        """) \
-        .add_arguments(cadence_public_keys) \
+        """
+        )
+        .add_arguments(cadence_public_keys)
         .add_arguments(cadence_contracts)
+    )
 
     return tx
