ee8ba8611d3bf2ce271c8b2d4e3552679a973578
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-08-12 23:06:08 +0200

    location tests

---
+++ tests/cadence/location_test.py
@@ -0,0 +1,50 @@
+import unittest
+from dataclasses import dataclass
+
+from flow_py_sdk import cadence
+
+
+class TestLocation(unittest.TestCase):
+    def testLocationId(self):
+        location_str = "0102"
+        type_id_str = "something"
+
+        @dataclass
+        class TestCase:
+            location: cadence.Location
+            expected_id: str
+            expected_type_id: str
+
+            def __init__(self, loc, expected_id, expected_type_id) -> None:
+                super().__init__()
+                self.location = loc
+                self.expected_id = expected_id
+                self.expected_type_id = expected_type_id
+
+        cases: list[TestCase] = [
+            TestCase(
+                cadence.ScriptLocation(location_str),
+                f"s.{location_str}",
+                f"s.{location_str}.{type_id_str}",
+            ),
+            TestCase(
+                cadence.AddressLocation(cadence.Address.from_hex("0x01"), location_str),
+                f"A.0000000000000001.{location_str}",
+                f"A.0000000000000001.{type_id_str}",
+            ),
+            TestCase(
+                cadence.StringLocation(location_str),
+                f"S.{location_str}",
+                f"S.{location_str}.{type_id_str}",
+            ),
+            TestCase(cadence.FlowLocation(), "flow", f"flow.{type_id_str}"),
+        ]
+
+        for case in cases:
+            with self.subTest(msg=f"{case} id"):
+                location_id = case.location.id()
+                self.assertEqual(case.expected_id, location_id)
+
+            with self.subTest(msg=f"{case} type_id"):
+                location_id = case.location.type_id(type_id_str)
+                self.assertEqual(case.expected_type_id, location_id)
