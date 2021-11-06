6e3eb2bd49bf9c1dd00792e2e04b824b3fc259f8
Janez Podhostnik <janez.podhostnik@gmail.com>
2021-11-06 16:26:08 +0000

    Fix integration CI

--- examples/main.py
+++ examples/main.py
@@ -1,25 +1,28 @@
 import asyncio
 import logging
 import sys
+from typing import Annotated
 
 from examples.common import Config, example_registry
 
 log = logging.getLogger(__name__)
 
 
-async def run_async(ctx: Config, examples: list[str]):
+async def run_async(ctx: Config, examples: list[str]) -> Annotated[bool, "Success"]:
     if not examples:
-        await example_registry.run_all(ctx)
+        return await example_registry.run_all(ctx)
     else:
+        success = True
         for ex in examples:
-            await example_registry.run(ctx, ex)
+            success = success and await example_registry.run(ctx, ex)
+        return success
 
 
 def run():
 
     # last index of string "examples"
     example_index = sys.argv.index("examples")
-    examples = sys.argv[example_index + 1 :]
+    examples = sys.argv[example_index + 1:]
 
     ctx = Config()
     loop = asyncio.new_event_loop()
