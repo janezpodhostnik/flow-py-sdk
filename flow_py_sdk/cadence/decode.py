c5be9463fa57c0d256a35b3b0b173017a765bbf2
Lea Lobanov <lea.lobanov@icloud.com>
2024-11-02 00:43:55 +0900

    Clean up

--- flow_py_sdk/cadence/decode.py
+++ flow_py_sdk/cadence/decode.py
@@ -40,21 +40,6 @@ def decode(obj: dict[Any, Any]) -> Union[Value, Kind, dict]:
                 decoder = _cadence_decoders[type_]
                 return decoder(obj)
 
-        # # Recursively handle nested structures
-        # if isinstance(obj, dict):
-        #     for key, value in obj.items():
-        #         if isinstance(value, dict):
-        #             obj[key] = decode(value)  # Recursive call for nested dict
-        #         elif isinstance(value, list):
-        #             obj[key] = [
-        #                 decode(item) for item in value
-        #             ]  # Recursive list handling
-        #
-        # elif isinstance(obj, list):
-        #     obj = [
-        #         decode(item) for item in obj
-        #     ]  # Handle obj itself as a list if list type
-
         return obj  # Return the object if no decoder applies
 
     except KeyError as e:
